97ad2b8ef69369ce16be1f061d0dc14cb14766f6
Nebulous8 <oscar@wulumulu.com>
2018-11-03 04:51:49 +0000

    Update fakedeath.ts

--- fakedeath.ts
+++ fakedeath.ts
@@ -1,1 +1,7 @@
-<iframe width="560" height="315" src="https://www.youtube.com/embed/0RqEllIU_OU" frameborder="0" allowfullscreen></iframe>
+
+const magik = magikcraft.io;
+
+function _fb(n=10) {
+    magik.doNTimes(()=> magik.infierno(), n, 300);
+    magik.dixit('FIREBALLZ!!');
+}
