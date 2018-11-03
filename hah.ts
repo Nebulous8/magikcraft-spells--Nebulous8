5a85ca4621c6c34121717267812aebaa34fd6de0
Nebulous8 <oscar@wulumulu.com>
2018-11-03 03:51:07 +0000

    Update hah.ts

--- hah.ts
+++ hah.ts
@@ -5,9 +5,6 @@ const items = require('items')
 
 function main() {
     const myInventory = inventory(self)
-    myInventory.add( items.bucket(64) )
-    myInventory.add( items.diamondSword(1))
-    myInventory.add( items.diamondPickaxe(1))
     myInventory.add( items.diamondBlock(64))
 
 }
